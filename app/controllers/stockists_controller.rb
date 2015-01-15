02f6ce0f3e40a2c877422609d165932fd92a4582
Jonny Dalgliesh <fighella@gmail.com>
2015-01-16 10:28:56 +1100

    Seeds

--- app/controllers/stockists_controller.rb
+++ app/controllers/stockists_controller.rb
@@ -6,4 +6,12 @@ class StockistsController < ApplicationController
       marker.lng stockist.longitude
     end
   end
+
+  def show
+    @stockist = Stockist.find(params[:id])
+    @hash = Gmaps4rails.build_markers([@stockist]) do |stockist, marker|
+      marker.lat stockist.latitude
+      marker.lng stockist.longitude
+    end
+  end
 end
