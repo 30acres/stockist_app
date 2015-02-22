f7cdee2208c51f30f704db89a4a936801697739b
Jonny Dalgliesh <fighella@gmail.com>
2015-02-22 16:23:30 +1100

    Stockist Address Improved

--- app/models/stockist.rb
+++ app/models/stockist.rb
@@ -27,7 +27,7 @@ class Stockist < ActiveRecord::Base
   end
 
   def address
-    [street_address.titleize.gsub(', ','/'), city.titleize.gsub(',',' '), state.upcase, country.to_s.titleize].compact.map{|e| e.to_s }.join(', ')
+    [street_address.titleize.gsub(', ','/'), city.titleize.gsub(',',' ').strip, state.upcase, country.to_s.titleize].compact.map{|e| e.to_s }.join(', ')
   end
 
   def is_mapped?
