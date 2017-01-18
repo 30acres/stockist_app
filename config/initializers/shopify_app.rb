8a531d6514b29f9e694c8455b48341940ad13b99
hazzajerdo <hazza_jerdo91@hotmail.com>
2017-01-18 12:42:43 +1100

    shop_app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.api_key = "<api_key>"
+  config.api_key = "8e098c9a63289a8cdfafcefa15750ddc"
   config.secret = "298cef654795d2722c2a891a0dae3b11"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
