c475fb2dc72928ad6dc19eece65488fb86bd7c62
Jonny Dalgleish <fighella@gmail.com>
2017-02-02 14:48:44 +1100

    Change something

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,127 @@
 class ContactsController < ApplicationController
   layout false
   # before_filter :skip_authentication
+  # {
+#    "cursor": null,
+#    "count": null,
+#    "id": 4828497193205760,
+#    "type": "PERSON",
+#    "created_time": 1484696869,
+#    "updated_time": 1484696870,
+#    "last_contacted": 0,
+#    "last_emailed": 0,
+#    "last_campaign_emaild": 0,
+#    "last_called": 0,
+#    "viewed_time": 0,
+#    "viewed": {
+#       "viewed_time": 1485494902244,
+#       "viewer_id": 6377278012391424
+#    },
+#    "star_value": 0,
+#    "lead_score": 0,
+#    "klout_score": "",
+#    "tags": [
+#       "Shopify contact",
+#       "Goddess Mascara Black",
+#       "Goddess Mascara Babylon Brown"
+#    ],
+#    "tagsWithTime": [
+#       {
+#          "cursor": null,
+#          "count": null,
+#          "tag": "Shopify contact",
+#          "createdTime": 1484696869156,
+#          "availableCount": 0,
+#          "entity_type": "tag"
+#       },
+#       {
+#          "cursor": null,
+#          "count": null,
+#          "tag": "Goddess Mascara Black",
+#          "createdTime": 1484696869750,
+#          "availableCount": 0,
+#          "entity_type": "tag"
+#       },
+#       {
+#          "cursor": null,
+#          "count": null,
+#          "tag": "Goddess Mascara Babylon Brown",
+#          "createdTime": 1484696870069,
+#          "availableCount": 0,
+#          "entity_type": "tag"
+#       }
+#    ],
+#    "properties": [
+#       {
+#          "type": "SYSTEM",
+#          "name": "first_name",
+#          "subtype": null,
+#          "value": "Tracey "
+#       },
+#       {
+#          "type": "SYSTEM",
+#          "name": "last_name",
+#          "subtype": null,
+#          "value": "van Hooydonk"
+#       },
+#       {
+#          "type": "SYSTEM",
+#          "name": "email",
+#          "subtype": "home",
+#          "value": "vanhooydonk@bigpond.com"
+#       },
+#       {
+#          "type": "SYSTEM",
+#          "name": "phone",
+#          "subtype": "home",
+#          "value": "0408 517 645"
+#       },
+#       {
+#          "type": "SYSTEM",
+#          "name": "address",
+#          "subtype": "home",
+#          "value": {
+#             "zip": "3910",
+#             "address": "367 North Road",
+#             "state": "Victoria",
+#             "countryname": "Australia",
+#             "city": "Langwarrin",
+#             "country": "AU"
+#          }
+#       },
+#       {
+#          "type": "SYSTEM",
+#          "name": "shopifySyncId",
+#          "subtype": "home",
+#          "value": "1724644677"
+#       }
+#    ],
+#    "campaignStatus": [],
+#    "widget_properties": null,
+#    "entity_type": "contact_entity",
+#    "source": "shopify sync",
+#    "contact_company_id": null,
+#    "unsubscribeStatus": [],
+#    "emailBounceStatus": [],
+#    "formId": 0,
+#    "browserId": [],
+#    "guid": null,
+#    "lead_source_id": 0,
+#    "lead_status_id": 0,
+#    "is_lead_converted": false,
+#    "lead_converted_time": 0,
+#    "owner": {
+#       "id": 6377278012391424,
+#       "domain": "racosmetics",
+#       "email": "jeff@eyeofhoruscosmetics.com",
+#       "phone": "",
+#       "name": "Jeff Spierings",
+#       "pic": "https://s3.amazonaws.com/agilecrm/panel/uploaded-logo/1442880649869?id=upload-in-modal",
+#       "schedule_id": "Jeff_Spierings",
+#       "calendar_url": "https://racosmetics.agilecrm.com/calendar/Jeff_Spierings",
+#       "calendarURL": "https://racosmetics.agilecrm.com/calendar/Jeff_Spierings"
+#    }
+# }
 
   def show
     
